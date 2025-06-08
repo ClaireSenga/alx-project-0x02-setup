d4774e0f6c237c11582840288bac9695d76e9753
Infdesk025 <infdesk025@Infdesk025s-MacBook-Air.local>
2025-06-08 16:48:51 +0200

    16:48

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -5,6 +5,13 @@ export interface ButtonProps {
   onClick?: () => void;
 }
 
+export interface PostProps {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+}
+
 export interface UserProps {
   id: number;
   name: string;
